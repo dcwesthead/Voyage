83cfe87a475346ab5319c050abe2fc9a02b461ea
dcwesthead <d.c.westhead@gmail.com>
2016-06-12 00:41:45 +0100

    Updated unit tests & integration tests

--- Voyage.Integration/Voyage.Integration.Application.UnitTests/CreateHomeCommandUnitTests.cs
+++ Voyage.Integration/Voyage.Integration.Application.UnitTests/CreateHomeCommandUnitTests.cs
@@ -2,6 +2,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
 using Voyage.Integration.Commands;
+using Voyage.Integration.Application.Interfaces.Repositories;
+using Voyage.Integration.Application.Interfaces.Adapters;
+using Voyage.Integration.ServiceContracts.Requests;
+using Voyage.Integration.ServiceContracts.Responses;
 
 namespace Voyage.Integration.Application.UnitTests
 {
@@ -10,18 +14,41 @@ namespace Voyage.Integration.Application.UnitTests
     {
         private CreateHomeCommand _command;
         private IHomeRepository _homeRepository;
+        private ICreateHomeAdapter _createHomeAdapter;
 
         [TestInitialize]
         public void TestInitialize()
         {
             _homeRepository = Substitute.For<IHomeRepository>();
+            _createHomeAdapter = Substitute.For<ICreateHomeAdapter>();
         }
 
         [TestMethod]
         public void WhenHomeDetailsAreSavedForANewHomeItIsCreatedInDataStorage()
         {
-            _command = new CreateHomeCommand(_homeRepository);
+            var expectedName = "Test Home";
+            var expectedExecutingUser = Environment.UserName;
+            var expectedResponse = new CreateHomeResponse
+            {
+                HomeId = 1,
+                Success = true
+            };
 
+            _createHomeAdapter.CreateHome(Arg.Is<string>(expectedName), Arg.Is<string>(expectedExecutingUser)).Returns<int>(expectedResponse.HomeId);
+            _homeRepository.CreateHome(Arg.Is<string>(expectedName), Arg.Is<string>(expectedExecutingUser)).Returns<int>(expectedResponse.HomeId);
+            
+            _command = new CreateHomeCommand(_createHomeAdapter, _homeRepository);
+
+            var request = new CreateHomeRequest
+            {
+                Name = expectedName,
+                ExecutingUser = expectedExecutingUser
+            };
+
+            var response = _command.Execute(request);
+
+            Assert.AreEqual(expectedResponse.Success, response.Success);
+            Assert.AreEqual(expectedResponse.HomeId, response.HomeId);
         }
 
         
